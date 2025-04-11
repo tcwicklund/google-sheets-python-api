62681dc16cde1ef42be6897ed8234987de7153a3
Travis <tcwicklund@gmail.com>
2025-04-11 02:09:05 +0000

    add credentials.b64 and decode logic to remove dependency on credentials.json

--- app/dependencies/sheets.py
+++ app/dependencies/sheets.py
@@ -1,8 +1,15 @@
 from google.oauth2.service_account import Credentials
 from app.services.google_sheets import GoogleSheetsService
+import base64
+import json
 
-creds = Credentials.from_service_account_file(
-    "app/credentials/credentials.json",
+# Load the credentials from the base64 encoded file
+with open("app/credentials/credentials.b64", "r") as file:
+    encoded_credentials = file.read()
+info = json.loads(base64.b64decode(encoded_credentials))
+
+creds = Credentials.from_service_account_info(
+    info,
     scopes=[
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive",
